5a31fe2577f5e54c0941510c7b1787f5479d7183
Gerard Crockenberg <gmahred@gmail.com>
2019-12-11 17:23:01 -0500

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import PictureComponent from "./components/PictureComponent/PictureComponent";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <PictureComponent/>
     </div>
   );
 }
